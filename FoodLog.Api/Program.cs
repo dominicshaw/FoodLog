d7d03bd15cfb111060549fcac71cbeabb6cf2f3d
Dominic Shaw <shawd@ttint.com>
2019-01-17 16:00:35 +0000

    added windows-service-hosting

--- FoodLog.Api/Program.cs
+++ FoodLog.Api/Program.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.Logging;
 
 namespace FoodLog.Api
 {
@@ -7,13 +12,38 @@ namespace FoodLog.Api
     {
         public static void Main(string[] args)
         {
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                Directory.SetCurrentDirectory(pathToContentRoot);
+            }
+
+            var builder = CreateWebHostBuilder(
+                args.Where(arg => arg != "--console").ToArray());
+
+            var host = builder.Build();
+
+            if (isService)
+                host.RunAsService();
+            else
+                host.Run();
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseKestrel()
-                .UseIISIntegration()
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddEventLog();
+                })
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    // Configure the app here.
+                })
                 .UseUrls("http://+:5000/")
                 .UseStartup<Startup>();
     }
